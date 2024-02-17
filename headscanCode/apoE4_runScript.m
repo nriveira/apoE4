7f58c139a82b933150a8d221f2230009b8956f55
nick <nickriveira@utexas.edu>
2024-02-16 20:06:43 -0800

    Update apoE4_runScript.m

--- headscanCode/apoE4_runScript.m
+++ headscanCode/apoE4_runScript.m
@@ -9,15 +9,15 @@ param.radial_thresh = 5;
 param.pause_thresh = 6;
 param.v_max = 50;      
 
-saveDir = '../figures/poster';
-fileLoc = dir('../headscanData/iteration1');
-for d = 3:length(fileLoc)
-    extractBefore(fileLoc(d).name, 'DLC_resnet50');
-    plot_headscans([fileLoc(d).folder filesep fileLoc(d).name], param)
-    figure(3);
-    saveas(gcf, [saveDir filesep extractBefore(fileLoc(d).name, 'DLC_resnet50')], 'png')
-    saveas(gcf, [saveDir filesep extractBefore(fileLoc(d).name, 'DLC_resnet50')], 'epsc')
-end
+% saveDir = '../figures/poster';
+% fileLoc = dir('../headscanData/iteration1');
+% for d = 3:length(fileLoc)
+%     extractBefore(fileLoc(d).name, 'DLC_resnet50');
+%     plot_headscans([fileLoc(d).folder filesep fileLoc(d).name], param)
+%     figure(3);
+%     saveas(gcf, [saveDir filesep extractBefore(fileLoc(d).name, 'DLC_resnet50')], 'png')
+%     saveas(gcf, [saveDir filesep extractBefore(fileLoc(d).name, 'DLC_resnet50')], 'epsc')
+% end
 
 %% Plot all headscans
 %       Calls plot_all_headscans function that plots all extracted data
